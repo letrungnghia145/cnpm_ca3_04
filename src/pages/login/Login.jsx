52040abfd7396c180a775181f47df571399b28e6
ThaiNgocLeDuy <64624794+ThaiNgocLeDuy@users.noreply.github.com>
2020-08-04 10:32:56 +0700

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import backgroundLogin from './../../assets/img/background-login.jpg'
-import './../../assets/css/login-style.css';
-import logo from './../../assets/img/logo.png';
+import backgroundLogin from "./../../assets/img/background-login.jpg";
+import "./../../assets/css/login-style.css";
+import logo from "./../../assets/img/logo.png";
 
 export const Login = () => {
   return (
@@ -14,7 +14,7 @@ export const Login = () => {
       <div className="container-login100">
         <div className="wrap-login100">
           <form className="login100-form validate-form">
-            <span className="login100-form-title p-b-34">Account Login</span>
+            <span className="login100-form-title p-b-34">Login</span>
             <div
               className="wrap-input100 rs1-wrap-input100 validate-input m-b-20"
               data-validate="Type user name"
@@ -47,11 +47,23 @@ export const Login = () => {
             </div>
             <div className="w-full text-center p-t-27 p-b-239">
               <span className="txt1">Forgot </span>
-              <a href="/" className="txt2">
+              <a href="/forgot" className="txt2">
                 User name / password?
               </a>
             </div>
-            <div className="w-full text-center">
+            <div className="w-full text-center" 
+            style={{backgroundColor: "#D74135", marginBottom: "10px", padding: "10px 0", borderRadius: "10px"}}>
+              <a style={{color: "white"}} href="https://www.facebook.com/dialog/oauth?client_id=180439422588509&redirect_uri=https://localhost:8443/login-facebook">
+                Login Google
+              </a>
+            </div>
+            <div className="w-full text-center" 
+            style={{backgroundColor: "#4267b2", padding: "10px 0", borderRadius: "10px"}}>
+              <a style={{color: "white"}} href="https://www.facebook.com/dialog/oauth?client_id=180439422588509&redirect_uri=https://localhost:8443/login-facebook">
+                Login Facebook
+              </a>
+            </div>
+            <div className="w-full text-center" style={{paddingTop: "15px"}}>
               <a href="/register" className="txt3">
                 Register
               </a>
