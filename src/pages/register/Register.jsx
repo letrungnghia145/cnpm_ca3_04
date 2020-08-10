e17fc8548288bb3a27a6f86f13e554d2e96016e6
ThaiNgocLeDuy <64624794+ThaiNgocLeDuy@users.noreply.github.com>
2020-08-10 21:50:15 +0700

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import logo from './../../assets/img/logo.png';
-import pgwind2 from './../../assets/img/pgnwind2.png';
-import pgwind from './../../assets/img/pngwing.com.png';
-import './../../assets/css/regi-style.css';
-
+import { useForm } from "react-hook-form";
+import logo from "./../../assets/img/logo.png";
+import pgwind2 from "./../../assets/img/pgnwind2.png";
+import pgwind from "./../../assets/img/pngwing.com.png";
+import "./../../assets/css/regi-style.css";
 
 export const Register = () => {
+
+  const { register, handleSubmit, errors, watch } = useForm();
+
+  function onSubmit(data) {
+    console.log("Data submitted: ", data);
+  }
+
   return (
     <div className="wrapper">
       <div className="logo__back">
@@ -16,7 +23,7 @@ export const Register = () => {
       <div className="inner">
         <img src={pgwind} alt="" className="image-1" />
 
-        <form className="form__">
+        <form className="form__" onSubmit={handleSubmit(onSubmit)}>
           <h3 className="h3__">Register</h3>
           <div className="form-holder">
             <span className="lnr lnr-user" />
@@ -25,17 +32,37 @@ export const Register = () => {
               name="username"
               className="form-control"
               placeholder="Username"
+              ref={register({
+                required: "Enter your username",
+                minLength: {
+                  value: 6,
+                  message: "Your username is to short, min length is 6"
+                }
+               })}
             />
           </div>
+          {errors.username && <p style={{color: "red"}} className="error">{errors.username.message}</p>}
           <div className="form-holder">
             <span className="lnr lnr-phone-handset" />
             <input
-              type="text"
+              type="number"
               name="tel"
               className="form-control"
               placeholder="Phone Number"
+              ref={register({
+                required: "Enter your tel",
+                minLength: {
+                  value: 9,
+                  message: "Your tel is to short, min length is 9"
+                },
+                maxLength: {
+                  value: 11,
+                  message: "Your tel is to long, max length is 11"
+                }
+               })}
             />
           </div>
+          {errors.tel && <p style={{color: "red"}} className="error">{errors.tel.message}</p>}
           <div className="form-holder">
             <span className="lnr lnr-envelope" />
             <input
@@ -43,13 +70,24 @@ export const Register = () => {
               name="mail"
               className="form-control"
               placeholder="Mail"
+              ref={register({
+                required: "Enter your mail",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Enter a valid mail address"
+                },
+              })}
             />
           </div>
+          {errors.mail && <p style={{color: "red"}} className="error">{errors.mail.message}</p>}
           <div className="form-holder">
             <select
               className="form-control"
               id="exampleFormControlSelect1"
               name="genderSl"
+              ref={register({ 
+                required: true
+               })}
             >
               <option value={0}>Gender</option>
               <option value={1}>Male</option>
@@ -57,6 +95,7 @@ export const Register = () => {
               <option value={3}>Other</option>
             </select>
           </div>
+          {errors.genderSl && <p style={{color: "red"}} className="error">{errors.genderSl.message}</p>}
           <div className="form-holder">
             <span className="lnr lnr-lock" />
             <input
@@ -64,8 +103,20 @@ export const Register = () => {
               name="password"
               className="form-control"
               placeholder="Password"
+              ref={register({
+                required: "Enter your password",
+                minLength: {
+                  value: 8,
+                  message: "Your password is to short, min length is 8"
+                },
+                pattern : {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                  message: "Your password must have atleast letter, number"
+                }
+              })}
             />
           </div>
+          {errors.password && <p style={{color: "red"}} className="error">{errors.password.message}</p>}
           <div className="form-holder">
             <span className="lnr lnr-lock" />
             <input
@@ -73,16 +124,20 @@ export const Register = () => {
               name="repassword"
               className="form-control"
               placeholder="Confirm Password"
+              ref={register({
+                validate: (value) => value === watch('password') || "The passwords do not match"
+              })}
             />
           </div>
+          {errors.repassword && <p style={{color: "red"}} className="error">{errors.repassword.message}</p>}
+          <div className="form-holder"></div>
           <button type="submit" className="btn__">
             <span>Register</span>
-          </button >
+          </button>
           <a href="/login" className="txt3">
             Have a account ? Sign in NOW
           </a>
         </form>
-
         <img src={pgwind2} alt="" className="image-2" />
       </div>
     </div>
